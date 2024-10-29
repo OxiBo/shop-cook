15360b5bbc73a1dda33d141c7b8953e4c70d1c57
OxiBo <xenos2000@ukr.net>
2024-10-29 00:03:31 -0400

    Commit everything

--- services/mailer.js
+++ services/mailer.js
@@ -1,8 +1,8 @@
 // https://sendgrid.com/docs/for-developers/sending-email/quickstart-nodejs/#complete-code-block
 
-const keys = require("../config/keys"),
-  sgMail = require("@sendgrid/mail"),
-  emailTemplate = require("./emailTemplate");
+const keys = require('../config/keys'),
+  sgMail = require('@sendgrid/mail'),
+  emailTemplate = require('./emailTemplate');
 sgMail.setApiKey(keys.sendGridKey);
 
 module.exports = (shoppingList, sendTo, sendFrom) => {
@@ -11,14 +11,14 @@ module.exports = (shoppingList, sendTo, sendFrom) => {
     to: sendTo, // Change to your recipient
     from: sendFrom, // Change to your verified sender
     subject: `Shopping List, ${sentAt.toDateString()} `,
-    text: "Shopping List",
+    text: 'Shopping List',
     html: emailTemplate(shoppingList, sentAt),
   };
 
   sgMail
     .send(msg)
     .then(() => {
-      console.log("Email sent");
+      console.log('Email sent'); // res.status(200).json({ status: 'Ok' });
     })
     .catch((error) => {
       console.error(error);
